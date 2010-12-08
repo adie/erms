95b5950953a35eddbc57d48a05a05a42f149bff6
ADie <antondie@gmail.com>
2010-12-08 15:45:02 +0300

    removed cookies; now requiring session_id parameter

--- src/erms_api.erl
+++ src/erms_api.erl
@@ -6,7 +6,7 @@
 -module(erms_api).
 -author("Anton Dieterle <antondie@gmail.com>").
 
--export([process_api/2, login_cookies/1]).
+-export([process_api/2]).
 
 %% Processing API requests
 
@@ -16,74 +16,73 @@ process_api(Req, ["login",Login,Password]) ->
       list_to_binary(Password)) of
     true ->
       User = users:find_first({login, '=', Login}),
-      api_ok(Req, login_cookies(users:id(User)), <<"You're logged in now">>);
+      SessionId = erms_session_store:create(users:id(User)),
+      return_ok(Req, {struct, [
+            {message, <<"You're logged in now">>},
+            {session_id, list_to_binary(SessionId)}
+          ]});
     _ ->
-      api_error(Req, <<"Wrong username or password">>)
+      return_error(Req, <<"Wrong username or password">>)
   end;
 
-process_api(Req, ["logout"]) ->
-  api_ok(Req, login_cookies("", ""), <<"You have logged out">>);
-
 process_api(Req, Params) ->
-  case Req:get_cookie_value("user_id") of
+  case proplists:get_value("session_id", Req:parse_qs()) of
     undefined ->
-      api_error(Req, <<"You need to authenticate first">>);
-    UserId ->
-      case users:find_first({id, '=', UserId}) of
-        undefined ->
-          api_error(Req, <<"You need to authenticate first">>);
-        User ->
-          case erms_session_store:read(Req:get_cookie_value("session_id")) of
-            not_found ->
-              api_error(Req, <<"You need to authenticate first.">>);
-            expired ->
-              api_error(Req, <<"Your session expired. Plase authenticate again.">>);
-            SessUserId ->
-              process_api(Req, Params, User)
-          end
-      end
+      return_error(Req, <<"You need to authenticate first">>);
+    SessionId ->
+      process_api(Req, Params, SessionId)
   end.
 
-process_api(Req, [Module,Function|Args], User) ->
+process_api(Req, ["logout"], SessionId) ->
+  erms_session_store:destroy(SessionId),
+  return_ok(Req, <<"You have logged out">>);
+
+process_api(Req, Params, SessionId) ->
+  io:format("SessionId: ~p~n", [SessionId]),
+  case erms_session_store:read(SessionId) of
+    not_found ->
+      return_error(Req, <<"You need to authenticate first.">>);
+    expired ->
+      return_error(Req, <<"Your session expired. Plase authenticate again.">>);
+    UserId ->
+      io:format("UserId: ~p~n", [UserId]),
+      process_api(Req, Params, SessionId, users:find_id(UserId))
+  end.
+
+process_api(Req, [Module,Function|Args], SessionId, User) ->
   M = list_to_atom(Module),
   F = list_to_atom(Function),
   case catch M:F(Args) of
     {'EXIT', {Error, _}} ->
-      api_error(Req, [<<"Error in API call:">>, Error]);
+      return_error(Req, [<<"Error in API call:">>, Error]);
     Result ->
-      api_ok(Req, {struct, [{result, Result}]})
+      return_ok(Req, {struct, [{result, Result}]})
   end;
 
-process_api(Req, [Module], User) ->
+process_api(Req, [Module], SessionId, User) ->
   M = list_to_atom(Module),
   case (catch M:api_functions()) of
     {'EXIT', {undef, _}} ->
-      api_error(Req, <<"No such module">>);
+      return_error(Req, <<"No such module">>);
     Funs ->
-      api_ok(Req, {struct, [{functions, Funs}]})
+      return_ok(Req, {struct, [{functions, Funs}]})
   end;
 
-process_api(Req, _Params, User) ->
-  api_ok(Req, [<<"Welcome to the API">>, users:fullname(User)]).
+process_api(Req, _Params, _SessionId, User) ->
+  return_ok(Req, [<<"Welcome to the API">>, users:fullname(User)]).
 
 %% Response generation
 
-api_ok(Req, Json) ->
-  api_ok(Req, [], Json).
+return_ok(Req, Json) ->
+  return_ok(Req, [], Json).
 
-api_ok(Req, Headers, Json) ->
-  Req:ok({"text/plain", Headers, mochijson2:encode(Json) }).
+return_ok(Req, Headers, {struct, _} = Json) ->
+  Req:ok({"text/plain", Headers, mochijson2:encode(Json) });
+return_ok(Req, Headers, Message) ->
+  return_ok(Req, Headers, {struct, [{message, Message}]}).
 
-api_error(Req, Error) ->
-  api_ok(Req, {struct, [{error, Error}]}).
-
-%% Using login information
-
-login_cookies(UserId) ->
-  login_cookies(UserId, erms_session_store:create(UserId)).
-login_cookies(UserId, SessionId) ->
-  [ mochiweb_cookies:cookie("user_id", UserId, [{path, "/"}]),
-    mochiweb_cookies:cookie("session_id", SessionId, [{path, "/"}]) ].
+return_error(Req, Error) ->
+  return_ok(Req, {struct, [{error, Error}]}).
 
 %%
 %% Tests
